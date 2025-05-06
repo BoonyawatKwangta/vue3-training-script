5b6a74f43f3394c7d723899a93d29c4cd62dc0e3
Bean Boonyawat <bean1888@outlook.com>
2025-05-06 22:08:19 +0700

    ีupdate script

--- src/views/BasicComposablePage.vue
+++ src/views/BasicComposablePage.vue
@@ -1,11 +1,36 @@
 <template>
-  <p>Position X: {{ mouse.x }}</p>
-  <p>Position Y: {{ mouse.y }}</p>
+  <!-- <p>Position X: {{ mouse.x }}</p>
+  <p>Position Y: {{ mouse.y }}</p> -->
+
+  <!-- เปรียบเทียบการ Sync กันของ Composable และ Pinia -->
+  <!-- Composables -->
+  <!-- Counter A -->
+  <div>
+    <p>Counter A: {{ a.count }}</p>
+    <button @click="a.increment">เพิ่ม A</button>
+  </div>
+
+  <!-- Counter B -->
+  <div style="margin-top: 1em;">
+    <p>Counter B: {{ b.count }}</p>
+    <button @click="b.increment">เพิ่ม B</button>
+  </div>
+
+  <!-- Pinia -->
+  <p>count: {{ counterStore.count }}</p>
+  <p>doubleCount: {{ counterStore.doubleCount }}</p>
+
+  <button @click="counterStore.increment"> + </button>
 </template>
 
 <script lang="ts" setup>
-  import { useMouse } from '@/composables/useMouse'
-  const mouse = useMouse()
+  import { useCounter } from '@/composables/useCounter'
+  import { useCounterStore } from '@/stores/counter'
+  const counterStore = useCounterStore()
+  // import { useMouse } from '@/composables/useMouse'
+  // const mouse = useMouse()
+  const a = useCounter()
+  const b = useCounter()
 </script>
 
 <style scoped>
